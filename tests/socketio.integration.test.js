87ffb436d02bfc8ef962a611ac37acd93855421c
Arik Mimran <arikm66@yahoo.com>
2026-02-01 20:10:24 +0200

    Use the dev server instead of spawn a dedicated test server

--- tests/socketio.integration.test.js
+++ tests/socketio.integration.test.js
@@ -7,23 +7,7 @@ require('dotenv').config();
 
 const out = fs.openSync('./server-test.log', 'a'); // 'a' for append mode
 let serverProcess;
-const SERVER_URL = 'http://localhost:6000'; // Points to the test server we are going to start using spawn
-
-beforeAll((done) => {
-  // Start the backend server as a child process
-  serverProcess = spawn('node', [path.join(__dirname, '../server.js')], {
-    env: { ...process.env, NODE_ENV: 'test', PORT: 6000 },
-    stdio: ['ignore', out, out],
-    cwd: path.join(__dirname, '..'),
-  });
-  // Wait for server to be ready
-  setTimeout(done, 3000); // Adjust delay if needed
-});
-
-afterAll((done) => {
-  if (serverProcess) serverProcess.kill();
-  setTimeout(done, 500);
-});
+const SERVER_URL = 'http://localhost:5000'; // Match the PORT in server.js
 
 test('should connect to backend Socket.io server', (done) => {
   const client = Client(SERVER_URL);
